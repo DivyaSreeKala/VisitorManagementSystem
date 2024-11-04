0f46b5aabe521aa454f3195a8c8d19f0a4ee1eab
DivyaSreeKala <divyasreekala99@gmail.com>
2024-11-04 17:33:19 +0530

    changes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -3,22 +3,39 @@ const express = require("express");
 
 const router = new express.Router();
 
-const adminData = require('../models/adminData');
+const adminModel = require('../models/adminData');
 
 router.use(express.json());
 router.use(express.urlencoded({extended : true}));
 
-
-router.get('/',async(req,res) => {
+router.get('/:id',async(req,res) => {
     try{
-    const data = await adminData.find();
-    console.log(data);
-    res.status(200).send(data);
+        const data = await adminModel.findById(req.params.id);
+        console.log(data);
+        res.status(200).send(data);
     }catch(err){
         res.status(404).send(err);
         console.log(err);
     }
-
 })
-
+router.put('/edit-admin/:id',async(req,res) => {
+    try{
+        const id = req.params.id;
+        const data = adminModel.findOneAndUpdate(id, req.body);
+        res.status(200).send('Post Succesfull');
+    }catch(err){
+        req.status(404).send(err)
+    }
+});
+router.post('/add',async(req, res) => {
+    try{
+        const requestData = req.body;
+        console.log(req.body)
+        const data = new adminModel(requestData); 
+        const savedData = await data.save();
+        res.status(200).send(savedData);
+    }catch(err){
+        res.status(404).send(err);
+    }
+})
 module.exports = router;
